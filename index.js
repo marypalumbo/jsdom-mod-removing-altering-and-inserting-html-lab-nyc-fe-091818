4bdfa671b59d7484e31ef06bf4281285e763664a
Mary Palumbo <mpalumbo@conductor.com>
2018-10-17 00:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
+element.remove(id='main');
+
+document.body.appendChild('h1#victory');
+let h1#victory = document.createElement('h1#victory');
+h1Tag.innerText = "Mary is the champion!"
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
