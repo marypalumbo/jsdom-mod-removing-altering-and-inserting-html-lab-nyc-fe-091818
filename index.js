12915790862186ec70f7affaeb4108c9baab0f5d
Mary Palumbo <mpalumbo@conductor.com>
2018-10-17 00:32:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
+//document.querySelector('#main').remove();
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = "Mary is the champion!"
+document.body.appendChild(header)
+const newHeader = document.querySelector('#victory');
+console.log(header)
